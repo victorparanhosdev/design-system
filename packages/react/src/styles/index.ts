d7a4ba4d9fd0126f2ed453a56800bce67ec8e52c
Victor Paranhos <victor_paranhos@hotmail.com>
2024-02-09 21:44:37 -0300

    initial project

---
+++ packages/react/src/styles/index.ts
@@ -0,0 +1,39 @@
+import {
+    colors,
+    fonts,
+    fontSizes,
+    fontWeights,
+    lineHeights,
+    radii,
+    space
+  } from '@system-ignite-ui/tokens'
+
+import { createStitches, defaultThemeMap } from '@stitches/react'
+
+export const {
+  styled,
+  css,
+  globalCss,
+  keyframes,
+  getCssText,
+  theme,
+  createTheme,
+  config,
+} = createStitches({
+  themeMap: {
+    ...defaultThemeMap,
+    height: 'space',
+    width: 'space',
+    minWidth: 'space'
+  },
+
+  theme: {
+    colors,
+    fontSizes,
+    fontWeights,
+    fonts,
+    lineHeights,
+    radii,
+    space,
+  },
+})

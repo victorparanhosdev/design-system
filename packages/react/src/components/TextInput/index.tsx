03f65b0c149308cf2ce727e1fb6bde1214ad59eb
Victor Paranhos <victor_paranhos@hotmail.com>
2024-02-16 09:20:21 -0300

    update

--- packages/react/src/components/TextInput/index.tsx
+++ packages/react/src/components/TextInput/index.tsx
@@ -1,12 +1,13 @@
 import { ComponentProps, forwardRef, ElementRef } from 'react'
 import { Input, Prefix, TextInputContainer } from './styles'
 
-export interface TextInputProps extends ComponentProps<typeof Input> {
+export interface TextInputProps extends Omit<ComponentProps<typeof Input>, 'size'>{
   prefix?: string
+  size?: 'sm' | 'md'
 }
 
 export const TextInput = forwardRef<ElementRef<typeof Input>, TextInputProps>(
-  ({ prefix, ...props }: TextInputProps, ref) => {
+  ({ prefix, size, ...props }: TextInputProps, ref) => {
     return (
       <TextInputContainer>
         {!!prefix && <Prefix>{prefix}</Prefix>}
